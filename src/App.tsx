31c25a29f148a1191c798887d48f97bf64bbbebc
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-10-05 15:51:02 -0700

    product cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ArrowUpRight, ChevronRight } from "lucide-react";
 
 export default function App() {
   return (
-    <main>
+    <main className="pb-20">
       {/* Hero  */}
       <div>
         <div className="flex flex-col gap-4 p-4 pb-10 pt-20 sm:flex-row sm:items-center sm:justify-between sm:px-8 sm:pb-20 xl:px-32">
@@ -50,7 +50,7 @@ export default function App() {
           </div>
         </div>
 
-        <div className="scrollbar-hide flex gap-2 overflow-x-auto p-4 sm:gap-6 sm:px-8 xl:pl-32">
+        <div className="scrollbar-hide flex gap-2 overflow-x-auto p-4 pb-10 sm:gap-6 sm:px-8 sm:pb-20 xl:pl-32">
           <div className="flex min-w-fit flex-col items-center gap-4">
             <img src="/mac.png" alt="mac" className="h-20 cursor-pointer" />
             <p className="font-semibold hover:cursor-pointer hover:underline">
@@ -138,6 +138,182 @@ export default function App() {
             <p className="font-semibold hover:cursor-pointer hover:underline">
               Apple Gift Card
             </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Product cards  */}
+      <div className="">
+        <p className="p-4 pb-8 text-2xl font-semibold tracking-wide text-zinc-500 sm:px-8 sm:text-3xl sm:leading-[1.10] xl:px-32">
+          <span className="text-foreground">The latest. </span>
+          Take a look at what's new, right now.
+        </p>
+
+        <div className="scrollbar-hide flex gap-4 overflow-x-auto p-4 sm:p-8 xl:gap-6 xl:pl-32">
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/iphone-16-pro.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-background xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">iPhone 16 Pro</p>
+              <p className="mb-1 font-semibold">Hello Apple Intelligence.</p>
+              <p className="text-sm">From $999 or $41.62/mo. for 24 mo.</p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/watch-s10.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-foreground xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">
+                Apple Watch Series 10
+              </p>
+              <p className="mb-1 font-semibold">Thinstant Classic.</p>
+              <p className="text-sm">From $399 or $33.25/mo. for 12 mo.</p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/iphone-16.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-background xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">iPhone 16</p>
+              <p className="mb-1 font-semibold">Hello Apple Intelligence.</p>
+              <p className="text-sm">From $799 or $33.29/mo. for 24 mo.</p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/watch-ultra.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-background xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">
+                Apple Watch Ultra 2
+              </p>
+              <p className="mb-1 font-semibold">New finish. Never quit.</p>
+              <p className="text-sm">From $799 or $66.58/mo. for 12 mo.</p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/airpods4.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-foreground xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">AirPods 4</p>
+              <p className="mb-1 font-semibold">Iconic. Now supersonic.</p>
+              <p className="text-sm">
+                Starting at $129 <br /> with Active Noise Cancellation $179
+              </p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/airpods-pro2.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-background xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">AirPods Pro 2</p>
+              <p className="mb-1 max-w-xs font-semibold">
+                With an all-in-one hearing health experience coming soon.
+              </p>
+              <p className="text-sm">$249 or $41.50/mo. for 6 mo.</p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/airpods-max.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-foreground xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">AirPods Max</p>
+              <p className="mb-1 font-semibold">
+                Five fresh colors. Bold sound.
+              </p>
+              <p className="text-sm">$549 or $91.50/mo. for 6 mo.</p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/vision-pro.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-foregound xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">Apple Vision Pro</p>
+              <p className="mb-1 font-semibold">Welcome to spacial computing.</p>
+              <p className="text-sm">From $3499 or $291.58/mo. for 12 mo.</p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/macbook-air.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-foreground xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">MacBook Air</p>
+              <p className="mb-1 font-semibold">Designed to go places.</p>
+              <p className="text-sm">From $999 or $83.25/mo. for 12 mo.</p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/ipad-air.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-foreground xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">iPad Air</p>
+              <p className="mb-1 font-semibold">Fresh air.</p>
+              <p className="text-sm">From $999 or $41.62/mo. for 24 mo.</p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/macbook-pro.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-foreground xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">Macbook Pro</p>
+              <p className="mb-1 font-semibold">Mind-blowing.<br/>Head-turning.</p>
+              <p className="text-sm">From $1599 or $133.25/mo. for 12 mo.</p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/ipad-pro.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
+            />
+            <div className="absolute top-[10%] px-8 text-background xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">iPad Pro</p>
+              <p className="mb-1 font-semibold">Thinpossible.</p>
+              <p className="text-sm">From $999 or $83.25/mo. for 12 mo.</p>
+            </div>
           </div>
         </div>
       </div>
