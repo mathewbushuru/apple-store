be15b850b78a75ed7c32295cb824a7cc9d025d43
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-10-05 17:05:11 -0700

    Help section

--- src/App.tsx
+++ src/App.tsx
@@ -143,13 +143,13 @@ export default function App() {
       </div>
 
       {/* Product cards  */}
-      <div className="">
-        <p className="p-4 pb-8 text-2xl font-semibold tracking-wide text-zinc-500 sm:px-8 sm:text-3xl sm:leading-[1.10] xl:px-32">
+      <div>
+        <p className="p-4 pb-2 text-2xl font-semibold tracking-wide text-zinc-500 sm:px-8 sm:text-3xl sm:leading-[1.10] xl:px-32">
           <span className="text-foreground">The latest. </span>
           Take a look at what's new, right now.
         </p>
 
-        <div className="scrollbar-hide flex gap-4 overflow-x-auto p-4 sm:p-8 xl:gap-6 xl:pl-32">
+        <div className="scrollbar-hide flex gap-4 overflow-x-auto p-4 pb-8 sm:p-8 sm:pb-12 xl:gap-6 xl:pl-32">
           <div className="relative min-w-fit cursor-pointer">
             <img
               src="/iphone-16-pro.jpeg"
@@ -257,9 +257,13 @@ export default function App() {
               alt="Iphone 16 Pro"
               className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[400px]"
             />
-            <div className="absolute top-[10%] px-8 text-foregound xl:space-y-2">
-              <p className="text-xl font-semibold xl:text-3xl">Apple Vision Pro</p>
-              <p className="mb-1 font-semibold">Welcome to spacial computing.</p>
+            <div className="text-foregound absolute top-[10%] px-8 xl:space-y-2">
+              <p className="text-xl font-semibold xl:text-3xl">
+                Apple Vision Pro
+              </p>
+              <p className="mb-1 font-semibold">
+                Welcome to spacial computing.
+              </p>
               <p className="text-sm">From $3499 or $291.58/mo. for 12 mo.</p>
             </div>
           </div>
@@ -298,7 +302,11 @@ export default function App() {
             />
             <div className="absolute top-[10%] px-8 text-foreground xl:space-y-2">
               <p className="text-xl font-semibold xl:text-3xl">Macbook Pro</p>
-              <p className="mb-1 font-semibold">Mind-blowing.<br/>Head-turning.</p>
+              <p className="mb-1 font-semibold">
+                Mind-blowing.
+                <br />
+                Head-turning.
+              </p>
               <p className="text-sm">From $1599 or $133.25/mo. for 12 mo.</p>
             </div>
           </div>
@@ -313,6 +321,107 @@ export default function App() {
               <p className="text-xl font-semibold xl:text-3xl">iPad Pro</p>
               <p className="mb-1 font-semibold">Thinpossible.</p>
               <p className="text-sm">From $999 or $83.25/mo. for 12 mo.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Help Section  */}
+      <div>
+        <p className="p-4 pb-2 text-2xl font-semibold tracking-wide text-zinc-500 sm:px-8 sm:text-3xl sm:leading-[1.10] xl:px-32">
+          <span className="text-foreground">Help is here. </span>
+          Whenever and however you need it.
+        </p>
+
+        <div className="scrollbar-hide flex gap-4 overflow-x-auto p-4 sm:p-8 sm:gap-6 xl:pl-32 xl:pr-24">
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/specialist.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[520px]"
+            />
+            <div className="absolute top-[5%] px-8 text-foreground xl:space-y-2">
+              <p className="mb-2 text-sm uppercase text-muted-foreground">
+                Apple Specialist
+              </p>
+              <p className="text-xl font-semibold sm:text-3xl">
+                Shop one on one with a Specialist. Online or in a store.{" "}
+              </p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/specialist-video.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[520px]"
+            />
+            <div className="absolute top-[5%] px-8 text-foreground xl:space-y-2">
+              <p className="mb-2 text-sm uppercase text-muted-foreground">
+                &nbsp;
+              </p>
+              <p className="text-xl font-semibold sm:text-3xl">
+                Shop with a Specialist over video.
+              </p>
+              <p className="max-w-xs font-light leading-tight sm:text-lg">
+                Choose your next device in a guided, one-way video session.
+              </p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/store-session.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[520px]"
+            />
+            <div className="absolute top-[5%] px-8 text-foreground xl:space-y-2">
+              <p className="mb-2 text-sm uppercase text-muted-foreground">
+                Today at Apple
+              </p>
+              <p className="text-xl font-semibold sm:text-3xl">
+                Join free sessions at your Apple Store.
+              </p>
+              <p className="font-light leading-tight sm:text-lg">
+                Learn about latest features and how to go further with your
+                apple devices.
+              </p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/personal-setup.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[520px]"
+            />
+            <div className="absolute top-[5%] px-8 text-foreground xl:space-y-2">
+              <p className="mb-2 text-sm uppercase text-muted-foreground">
+                &nbsp;
+              </p>
+              <p className="text-xl font-semibold sm:text-3xl">
+                Get to know your new device with Personal Setup.
+              </p>
+              <p className="font-light leading-tight sm:text-lg">
+                Let us guide you through setup, data transfer, and the latest
+                features in an online session.
+              </p>
+            </div>
+          </div>
+
+          <div className="relative min-w-fit cursor-pointer">
+            <img
+              src="/genius-bar.jpeg"
+              alt="Iphone 16 Pro"
+              className="h-[450px] w-[300px] rounded-2xl object-cover shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg sm:h-[500px] sm:w-[520px]"
+            />
+            <div className="absolute top-[5%] px-8 text-foreground xl:space-y-2">
+              <p className="mb-2 text-sm uppercase text-muted-foreground">
+                &nbsp;
+              </p>
+              <p className="max-w-sm text-xl font-semibold sm:text-3xl">
+                Get expert service and support at the Genius Bar.
+              </p>
             </div>
           </div>
         </div>
