4c957a9744e548e426f66dd5024f95e427f4ccf2
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-10-09 13:55:43 -0700

    info link item component

--- src/modules/home/pages/home-page.tsx
+++ src/modules/home/pages/home-page.tsx
@@ -1,11 +1,11 @@
-import HeroSection from "../components/hero-section";
-import ProductCardsSection from "../components/product-cards-section";
-import HelpSection from "../components/help-section";
-import WhyShopWithUsSection from "../components/why-shop-with-us-section";
-import AccessoriesSection from "../components/accessories-section";
-import MusicSection from "../components/music-section";
-import ServicesSection from "../components/services-section";
-import QuickLinksSection from "../components/quick-links-section";
+import HeroSection from "@/modules/home/components/hero-section";
+import ProductCardsSection from "@/modules/home/components/product-cards-section";
+import HelpSection from "@/modules/home/components/help-section";
+import WhyShopWithUsSection from "@/modules/home/components/why-shop-with-us-section";
+import AccessoriesSection from "@/modules/home/components/accessories-section";
+import MusicSection from "@/modules/home/components/music-section";
+import ServicesSection from "@/modules/home/components/services-section";
+import QuickLinksSection from "@/modules/home/components/quick-links-section";
 
 export default function HomePage() {
   return (
